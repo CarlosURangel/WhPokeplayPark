635a2369ed5a1cb29f0d0cf4b0cfe235b2b36664
CarlosURangel <crangel30@alumnos.uaq.mx>
2025-02-27 12:30:21 -0600

    rutas

---
+++ routes.js
@@ -0,0 +1,17 @@
+{
+    "routes": [
+      {
+        "route": "/clicker/*",
+        "serve": "/clicker/index.html"
+      },
+      {
+        "route": "/ticTacToe/*",
+        "serve": "/ticTacToe/index.html"
+      },
+      {
+        "route": "/wordleJS/*",
+        "serve": "/wordleJS/index.html"
+      }
+    ]
+  }
+  
